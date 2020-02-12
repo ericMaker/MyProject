9035e7ad1149cf17c3a1a7d2fe62bd4d8df8b3a5
ericMaker <ericmaker@163.com>
2020-02-12 20:37:31 +0800

    Initial commit

---
+++ Hello/src/HelloWord.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        ArrayList ls = new ArrayList();
+        List l = new ArrayList<>();
+        show();
+    }
+
+    public static void show(){
+        System.out.println("Show Method");
+        System.out.println("汉字效果");
+        System.out.println("汉字效果");
+        System.out.println("汉字效果");
+        ArrayList l1 = null;
+
+        System.out.println("汉字效果");
+
+
+    }
+}
---
+++ Hello/src/Test.java
@@ -0,0 +1,7 @@
+/**
+ * @author ericMaker
+ * @create 2020-02-11 16:59
+ *
+ */
+public class Test {
+}
